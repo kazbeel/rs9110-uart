c9d1d816cec91c1bab82838024a108e9ac17fa9a
diego.valladolid@gmail.com <diego.valladolid@gmail.com@f52250f4-abe1-f969-e49a-18531d42d78f>
2013-06-17 18:34:13 +0000

    ProcessMessage() does not need the size of the message. New helper methods (all private). Using the helper methods to optimize duplicate code.

--- trunk/source/RS9110_UART.cpp
+++ trunk/source/RS9110_UART.cpp
@@ -6,7 +6,6 @@
 #include <stdio.h>
 
 
-
 static const char *COMMAND[] =
 {
     "AT+RSI_BAND=",
@@ -57,12 +56,12 @@ static const char *CMD_END          = "\r\n";
 
 static const unsigned char CMD_RESP_OK_LEN      = strlen(CMD_RESP_OK);
 static const unsigned char CMD_RESP_ERROR_LEN	= strlen(CMD_RESP_ERROR);
+static const unsigned char CMD_RESP_READ_LEN    = strlen(CMD_RESP_READ);
 static const unsigned char CMD_END_LEN          = strlen(CMD_END);
-static const unsigned char CMD_READ_LEN         = strlen(CMD_RESP_READ);
 
 
-static bool IsValidString (const char *string, int maxLen = -1);
-static unsigned int SendByteStuffing (char *destination, unsigned int &dstSize, const char *source, unsigned int srcSize);
+static bool         IsValidString       (const char *string, int maxLen = -1);
+static unsigned int SendByteStuffing    (char *destination, unsigned int &dstSize, const char *source, unsigned int srcSize);
 
 
 
@@ -96,30 +95,25 @@ IPersistor * RS9110_UART::GetPersistor ()
 
 
 /*! @todo Should I return the response type? */
-bool RS9110_UART::ProcessMessage (char *message, unsigned int size)
+bool RS9110_UART::ProcessMessage (char *message)
 {
-    bool bRtn;
+    bool bRtn = true;
 
 
     ProcessResponseType(message);
 
+    _errorCode = ERROR_NONE;
+
     switch(GetResponseType())
     {
         case RESP_TYPE_OK:
-            _errorCode = ERROR_NONE;
-
-            if(size == ((unsigned int) (CMD_RESP_OK_LEN + CMD_END_LEN)))
-            {
-                return true;
-            }
+            /*! @todo Stub */
         break;
 
         case RESP_TYPE_ERROR:
-            if(size > ((unsigned int) (CMD_RESP_ERROR_LEN + CMD_END_LEN)))
+            if(strlen(message) > ((unsigned int) (CMD_RESP_ERROR_LEN + CMD_END_LEN)))
             {
-                _errorCode = (EErrorCode) ((unsigned char) message[CMD_RESP_ERROR_LEN]);
-
-                return true;
+                _errorCode = (EErrorCode) message[CMD_RESP_ERROR_LEN];
             }
         break;
 
@@ -131,20 +125,8 @@ bool RS9110_UART::ProcessMessage (char *message, unsigned int size)
             /*! @todo Stub */
         break;
 
-        default:
-            return false;
-        break;
-    }
-
-    /* Fetch information after OK response */
-    switch(_lastCommand)
-    {
-        case CMD_BAND:
-        case CMD_INIT:
-            /* Do nothing */
-        break;
-
-        case CMD_SCAN:
+        case RESP_TYPE_UNKNOWN:
+            /*! @todo Stub */
         break;
 
         default:
@@ -176,72 +158,37 @@ RS9110_UART::EErrorCode RS9110_UART::GetErrorCode ()
 
 bool RS9110_UART::Band (EBand eBand)
 {
-    bool bRtn;
-
-
     if(eBand >= BAND_MAX)
     {
         _lastCommand = CMD_MAX;
         return false;
     }
 
-    _snprintf_s(_buffer, sizeof(_buffer), "%s%d%s", COMMAND[CMD_BAND], eBand, CMD_END);
-
-    bRtn = _persistor->Write((unsigned char *) _buffer, strlen(_buffer));
-
-    _lastCommand = ((bRtn == true) ? CMD_BAND : CMD_MAX);
-
-    return bRtn;
+    return GenericCommandInt(CMD_BAND, eBand);
 }
 
 
 bool RS9110_UART::Init ()
 {
-    bool bRtn;
-
-
-    _snprintf_s(_buffer, sizeof(_buffer), "%s%s", COMMAND[CMD_INIT], CMD_END);
-
-    bRtn = _persistor->Write((unsigned char *) _buffer, strlen(_buffer));
-
-    _lastCommand = ((bRtn == true) ? CMD_INIT : CMD_MAX);
-
-    return bRtn;
+    return GenericCommand(CMD_INIT);
 }
 
 
 bool RS9110_UART::GetNumScanResults ()
 {
-    bool bRtn;
-
-
-    _snprintf_s(_buffer, sizeof(_buffer), "%s%s", COMMAND[CMD_GET_SCAN_RESULTS], CMD_END);
-
-    bRtn = _persistor->Write((unsigned char *) _buffer, strlen(_buffer));
-
-    _lastCommand = ((bRtn == true) ? CMD_GET_SCAN_RESULTS : CMD_MAX);
-
-    return bRtn;
+    return GenericCommand(CMD_GET_SCAN_RESULTS);
 }
 
 
 bool RS9110_UART::SetNumScanResults (unsigned char value)
 {
-    bool bRtn;
-
-
     if((value <= 0) || (value > MAX_NUM_SCAN_RESULTS))
     {
+        _lastCommand = CMD_MAX;
         return false;
     }
 
-    _snprintf_s(_buffer, sizeof(_buffer), "%s%d%s", COMMAND[CMD_SET_SCAN_RESULTS], value, CMD_END);
-
-    bRtn = _persistor->Write((unsigned char *) _buffer, strlen(_buffer));
-
-    _lastCommand = ((bRtn == true) ? CMD_SET_SCAN_RESULTS : CMD_MAX);
-
-    return bRtn;
+    return GenericCommandInt(CMD_SET_SCAN_RESULTS, value);
 }
 
 
@@ -276,46 +223,19 @@ bool RS9110_UART::Scan (unsigned char channel, const char *ssid)
 
 bool RS9110_UART::NextScan ()
 {
-    bool bRtn;
-
-
-    _snprintf_s(_buffer, sizeof(_buffer), "%s%s", COMMAND[CMD_NEXT_SCAN], CMD_END);
-
-    bRtn = _persistor->Write((unsigned char *) _buffer, strlen(_buffer));
-
-    _lastCommand = ((bRtn == true) ? CMD_NEXT_SCAN : CMD_MAX);
-
-    return bRtn;
+    return GenericCommand(CMD_NEXT_SCAN);
 }
 
 
 bool RS9110_UART::GetMACOfAPs ()
 {
-    bool bRtn;
-
-
-    _snprintf_s(_buffer, sizeof(_buffer), "%s%s", COMMAND[CMD_GET_MAC_APS], CMD_END);
-
-    bRtn = _persistor->Write((unsigned char *) _buffer, strlen(_buffer));
-
-    _lastCommand = ((bRtn == true) ? CMD_GET_MAC_APS : CMD_MAX);
-
-    return bRtn;
+    return GenericCommand(CMD_GET_MAC_APS);
 }
 
 
 bool RS9110_UART::GetNetworkType ()
 {
-    bool bRtn;
-
-
-    _snprintf_s(_buffer, sizeof(_buffer), "%s%s", COMMAND[CMD_GET_NETWORK_TYPE], CMD_END);
-
-    bRtn = _persistor->Write((unsigned char *) _buffer, strlen(_buffer));
-
-    _lastCommand = ((bRtn == true) ? CMD_GET_NETWORK_TYPE : CMD_MAX);
-
-    return bRtn;
+    return GenericCommand(CMD_GET_NETWORK_TYPE);
 }
 
 
@@ -327,15 +247,12 @@ bool RS9110_UART::SetNetworkType (ENetworkType eNWType, EIBSSType eIBSSType, uns
     switch(eNWType)
     {
         case NW_TYPE_INFRASTRUCTURE:
-            _snprintf_s(_buffer, sizeof(_buffer), "%s%s%s", COMMAND[CMD_SET_NETWORK_TYPE], NETWORK_TYPE_STR[NW_TYPE_INFRASTRUCTURE], CMD_END);
+            _snprintf_s(_buffer, sizeof(_buffer), "%s%s%s", COMMAND[CMD_SET_NETWORK_TYPE], NETWORK_TYPE_STR[eNWType], CMD_END);
         break;
 
         case NW_TYPE_IBSS:
-            _snprintf_s(_buffer, sizeof(_buffer), "%s%s,%d,%d%s", COMMAND[CMD_SET_NETWORK_TYPE], NETWORK_TYPE_STR[NW_TYPE_IBSS], eIBSSType, channel, CMD_END);
-        break;
-
         case NW_TYPE_IBSS_SEC:
-            _snprintf_s(_buffer, sizeof(_buffer), "%s%s,%d,%d%s", COMMAND[CMD_SET_NETWORK_TYPE], NETWORK_TYPE_STR[NW_TYPE_IBSS_SEC], eIBSSType, channel, CMD_END);
+            _snprintf_s(_buffer, sizeof(_buffer), "%s%s,%d,%d%s", COMMAND[CMD_SET_NETWORK_TYPE], NETWORK_TYPE_STR[eNWType], eIBSSType, channel, CMD_END);
         break;
 
         default:
@@ -354,37 +271,19 @@ bool RS9110_UART::SetNetworkType (ENetworkType eNWType, EIBSSType eIBSSType, uns
 
 bool RS9110_UART::PSK (const char *psk)
 {
-    bool bRtn;
-
-
     if(IsValidString(psk, MAX_PSK_LEN) == false)
     {
         _lastCommand = CMD_MAX;
         return false;
     }
 
-    _snprintf_s(_buffer, sizeof(_buffer), "%s%s%s", COMMAND[CMD_PSK], psk, CMD_END);
-
-    bRtn = _persistor->Write((unsigned char *) _buffer, strlen(_buffer));
-
-    _lastCommand = ((bRtn == true) ? CMD_PSK : CMD_MAX);
-
-    return bRtn;
+    return GenericCommandStr(CMD_PSK, psk);
 }
 
 
 bool RS9110_UART::AuthMode (EAuthMode eAuthMode)
 {
-    bool bRtn;
-
-
-    _snprintf_s(_buffer, sizeof(_buffer), "%s%d%s", COMMAND[CMD_AUTH_MODE], eAuthMode, CMD_END);
-
-    bRtn = _persistor->Write((unsigned char *) _buffer, strlen(_buffer));
-
-    _lastCommand = ((bRtn == true) ? CMD_AUTH_MODE : CMD_MAX);
-
-    return bRtn;
+    return GenericCommandInt(CMD_AUTH_MODE, eAuthMode);
 }
 
 
@@ -411,16 +310,7 @@ bool RS9110_UART::Join (const char *ssid, ETxRate eTxRate, ETxPower eTxPower)
 
 bool RS9110_UART::Disassociate ()
 {
-	bool bRtn;
-
-
-	_snprintf_s(_buffer, sizeof(_buffer), "%s%s", COMMAND[CMD_DISASSOCIATE], CMD_END);
-
-	bRtn = _persistor->Write((unsigned char *) _buffer, strlen(_buffer));
-
-	_lastCommand = ((bRtn == true) ? CMD_DISASSOCIATE : CMD_MAX);
-
-	return bRtn;
+    return GenericCommand(CMD_DISASSOCIATE);
 }
 
 
@@ -429,9 +319,6 @@ bool RS9110_UART::IPConfiguration (EDHCPMode eDHCPMode, const char *ipAddr, cons
 	bool bRtn = false;
 
 
-    /*! @todo   I think I should not check the format of the IP addresses. That must be done by the user.
-     *          The only think I must check is NULL and length = 0.
-     */
 	switch(eDHCPMode)
 	{
 		case DHCP_MANUAL:
@@ -474,11 +361,7 @@ bool RS9110_UART::OpenTcpSocket (const char *hostIpAddr, unsigned short targetPo
 	bool bRtn;
 
 
-    /*! @todo   I think I should not check the format of the IP addresses. That must be done by the user.
-     *          The only think I must check is NULL and length = 0.
-     */
-	if((IsValidString(hostIpAddr) == false) ||
-	   (!((localPort >= MIN_TCP_SOCKET_PORT) && (localPort <= MAX_TCP_SOCKET_PORT))))
+	if((IsValidString(hostIpAddr) == false) || (IsValidLocalTcpPort(localPort) == false))
 	{
         _lastCommand = CMD_MAX;
 		return false;
@@ -496,16 +379,7 @@ bool RS9110_UART::OpenTcpSocket (const char *hostIpAddr, unsigned short targetPo
 
 bool RS9110_UART::OpenListeningUdpSocket  (unsigned short localPort)
 {
-	bool bRtn;
-
-
-	_snprintf_s(_buffer, sizeof(_buffer), "%s%d%s", COMMAND[CMD_OPEN_LISTENING_UDP_SOCKET], localPort, CMD_END);
-
-	bRtn = _persistor->Write((unsigned char *) _buffer, strlen(_buffer));
-
-	_lastCommand = ((bRtn == true) ? CMD_OPEN_LISTENING_UDP_SOCKET : CMD_MAX);
-
-	return bRtn;
+    return GenericCommandInt(CMD_OPEN_LUDP_SOCKET, localPort);
 }
 
 
@@ -514,9 +388,6 @@ bool RS9110_UART::OpenUdpSocket (const char *hostIpAddr, unsigned short targetPo
 	bool bRtn;
 
 
-    /*! @todo   I think I should not check the format of the IP addresses. That must be done by the user.
-     *          The only think I must check is NULL and length = 0.
-     */
 	if(IsValidString(hostIpAddr) == false)
 	{
         _lastCommand = CMD_MAX;
@@ -535,64 +406,37 @@ bool RS9110_UART::OpenUdpSocket (const char *hostIpAddr, unsigned short targetPo
 
 bool RS9110_UART::OpenListeningTcpSocket  (unsigned short localPort)
 {
-	bool bRtn;
-
-
-	_snprintf_s(_buffer, sizeof(_buffer), "%s%d%s", COMMAND[CMD_OPEN_LISTENING_TCP_SOCKET], localPort, CMD_END);
-
-	bRtn = _persistor->Write((unsigned char *) _buffer, strlen(_buffer));
-
-	_lastCommand = ((bRtn == true) ? CMD_OPEN_LISTENING_TCP_SOCKET : CMD_MAX);
-
-	return bRtn;
+    return GenericCommandInt(CMD_OPEN_LTCP_SOCKET, localPort);
 }
 
 
 bool RS9110_UART::GetSocketStatus (unsigned char socketId)
 {
-	bool bRtn;
-
-
 	if(IsValidSocketId(socketId) == false)
 	{
         _lastCommand = CMD_MAX;
 		return false;
 	}
 
-    _snprintf_s(_buffer, sizeof(_buffer), "%s%d%s", COMMAND[CMD_GET_SOCKET_STATUS], socketId, CMD_END);
-
-	bRtn = _persistor->Write((unsigned char *) _buffer, strlen(_buffer));
-
-	_lastCommand = ((bRtn == true) ? CMD_GET_SOCKET_STATUS : CMD_MAX);
-
-	return bRtn;
+    return GenericCommandInt(CMD_GET_SOCKET_STATUS, socketId);
 }
 
 
 bool RS9110_UART::CloseSocket (unsigned char socketId)
 {
-	bool bRtn;
-
-
 	if(IsValidSocketId(socketId) == false)
 	{
         _lastCommand = CMD_MAX;
 		return false;
 	}
 
-	_snprintf_s(_buffer, sizeof(_buffer), "%s%d%s", COMMAND[CMD_CLOSE_SOCKET], socketId, CMD_END);
-
-	bRtn = _persistor->Write((unsigned char *) _buffer, strlen(_buffer));
-
-	_lastCommand = ((bRtn == true) ? CMD_CLOSE_SOCKET : CMD_MAX);
-
-	return bRtn;
+    return GenericCommandInt(CMD_CLOSE_SOCKET, socketId);
 }
 
 
 unsigned int RS9110_UART::Send (unsigned char socketId, ESocketType socketType, const char *hostIpAddr, unsigned short hostPort, const char *data, unsigned int dataSize)
 {
-	bool bRtn;
+	bool         bRtn;
     unsigned int sendLen;
     unsigned int maxDataLen;
 
@@ -640,148 +484,67 @@ unsigned int RS9110_UART::Send (unsigned char socketId, ESocketType socketType, 
 
 bool RS9110_UART::GetDNS (const char *domainName)
 {
-	bool bRtn;
-
-
     if(IsValidString(domainName) == false)
     {
         _lastCommand = CMD_MAX;
         return false;
     }
 
-	_snprintf_s(_buffer, sizeof(_buffer), "%s%s%s", COMMAND[CMD_GET_DNS], domainName, CMD_END);
-
-	bRtn = _persistor->Write((unsigned char *) _buffer, strlen(_buffer));
-
-	_lastCommand = ((bRtn == true) ? CMD_GET_DNS : CMD_MAX);
-
-	return bRtn;
+    return GenericCommandStr(CMD_GET_DNS, domainName);
 }
 
 
 bool RS9110_UART::GetFirmwareVersion ()
 {
-	bool bRtn;
-
-
-	_snprintf_s(_buffer, sizeof(_buffer), "%s%s", COMMAND[CMD_FW_VERSION], CMD_END);
-
-	bRtn = _persistor->Write((unsigned char *) _buffer, strlen(_buffer));
-
-	_lastCommand = ((bRtn == true) ? CMD_FW_VERSION : CMD_MAX);
-
-	return bRtn;
+    return GenericCommand(CMD_FW_VERSION);
 }
 
 
 bool RS9110_UART::GetNetworkParameters ()
 {
-	bool bRtn;
-
-
-	_snprintf_s(_buffer, sizeof(_buffer), "%s%s", COMMAND[CMD_GET_NETWORK_PARAMS], CMD_END);
-
-	bRtn = _persistor->Write((unsigned char *) _buffer, strlen(_buffer));
-
-	_lastCommand = ((bRtn == true) ? CMD_GET_NETWORK_PARAMS : CMD_MAX);
-
-	return bRtn;
+    return GenericCommand(CMD_GET_NETWORK_PARAMS);
 }
 
     
 bool RS9110_UART::Reset ()
 {
-	bool bRtn;
-
-
-	_snprintf_s(_buffer, sizeof(_buffer), "%s%s", COMMAND[CMD_RESET], CMD_END);
-
-    bRtn = _persistor->Write((unsigned char *) _buffer, strlen(_buffer));
-
-    _lastCommand = ((bRtn == true) ? CMD_RESET : CMD_MAX);
-
-    return bRtn;
+    return GenericCommand(CMD_RESET);
 }
 
 
 bool RS9110_UART::GetMACAddress ()
 {
-	bool bRtn;
-
-
-	_snprintf_s(_buffer, sizeof(_buffer), "%s%s", COMMAND[CMD_GET_MAC], CMD_END);
-
-    bRtn = _persistor->Write((unsigned char *) _buffer, strlen(_buffer));
-
-    _lastCommand = ((bRtn == true) ? CMD_GET_MAC : CMD_MAX);
-
-    return bRtn;
+    return GenericCommand(CMD_GET_MAC);
 }
 
 
 bool RS9110_UART::GetRSSI ()
 {
-	bool bRtn;
-
-
-	_snprintf_s(_buffer, sizeof(_buffer), "%s%s", COMMAND[CMD_GET_RSSI], CMD_END);
-
-    bRtn = _persistor->Write((unsigned char *) _buffer, strlen(_buffer));
-
-    _lastCommand = ((bRtn == true) ? CMD_GET_RSSI : CMD_MAX);
-
-    return bRtn;
+    return GenericCommand(CMD_GET_RSSI);
 }
 
 
 bool RS9110_UART::SaveConfiguration ()
 {
-	bool bRtn;
-
-
-	_snprintf_s(_buffer, sizeof(_buffer), "%s%s", COMMAND[CMD_SAVE_CONFIG], CMD_END);
-
-	bRtn = _persistor->Write((unsigned char *) _buffer, strlen(_buffer));
-
-	_lastCommand = ((bRtn == true) ? CMD_SAVE_CONFIG : CMD_MAX);
-
-	return bRtn;
+    return GenericCommand(CMD_SAVE_CONFIG);
 }
 
 
 bool RS9110_UART::Configuration (EConfiguration eConfig)
 {
-	bool bRtn;
-
-
     if(eConfig >= CONFIG_MAX)
     {
         _lastCommand = CMD_MAX;
         return false;
     }
 
-	_snprintf_s(_buffer, sizeof(_buffer), "%s%d%s", COMMAND[CMD_ENABLE_CONFIG], eConfig, CMD_END);
-
-	bRtn = _persistor->Write((unsigned char *) _buffer, strlen(_buffer));
-
-	_lastCommand = ((bRtn == true) ? CMD_ENABLE_CONFIG : CMD_MAX);
-
-	return bRtn;
+    return GenericCommandInt(CMD_ENABLE_CONFIG, eConfig);
 }
 
 
 bool RS9110_UART::GetConfiguration ()
 {
-	bool bRtn;
-
-
-	_snprintf_s(_buffer, sizeof(_buffer), "%s%s", COMMAND[CMD_GET_CONFIG], CMD_END);
-
-	bRtn = _persistor->Write((unsigned char *) _buffer, strlen(_buffer));
-
-	_lastCommand = ((bRtn == true) ? CMD_GET_CONFIG : CMD_MAX);
-
-	return bRtn;
+    return GenericCommand(CMD_GET_CONFIG);
 }
 
 
@@ -813,6 +576,57 @@ void RS9110_UART::ProcessResponseType (const char *message)
 inline bool RS9110_UART::IsValidSocketId (unsigned char socketId)
 {
     return ((socketId >= MIN_SOCKET_HANDLE) && (socketId <= MAX_SOCKET_HANDLE));
+}
+
+
+bool RS9110_UART::IsValidLocalTcpPort (unsigned short port)
+{
+    return ((port >= MIN_TCP_SOCKET_PORT) && (port <= MAX_TCP_SOCKET_PORT));
+}
+
+
+bool RS9110_UART::GenericCommand (ECommand command)
+{
+    bool bRtn;
+
+
+    _snprintf_s(_buffer, sizeof(_buffer), "%s%s", COMMAND[command], CMD_END);
+
+    bRtn = _persistor->Write((unsigned char *) _buffer, strlen(_buffer));
+
+    _lastCommand = ((bRtn == true) ? command : CMD_MAX);
+
+    return bRtn;
+}
+
+
+bool RS9110_UART::GenericCommandInt (ECommand command, int value)
+{
+    bool bRtn;
+
+
+    _snprintf_s(_buffer, sizeof(_buffer), "%s%d%s", COMMAND[command], value, CMD_END);
+
+    bRtn = _persistor->Write((unsigned char *) _buffer, strlen(_buffer));
+
+    _lastCommand = ((bRtn == true) ? command : CMD_MAX);
+
+    return bRtn;
+}
+
+
+bool RS9110_UART::GenericCommandStr (ECommand command, const char *str)
+{
+    bool bRtn;
+
+
+    _snprintf_s(_buffer, sizeof(_buffer), "%s%s%s", COMMAND[command], str, CMD_END);
+
+    bRtn = _persistor->Write((unsigned char *) _buffer, strlen(_buffer));
+
+    _lastCommand = ((bRtn == true) ? command : CMD_MAX);
+
+    return bRtn;
 }
 
 
