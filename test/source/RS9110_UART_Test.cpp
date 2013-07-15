32beea262b52f4e8d419b655979f11eb9910d2ab
diego.valladolid <diego.valladolid@gmail.com>
2013-07-15 06:02:53 +0000

    Added tests of GetResponse(). These tests are also a good way to learn how to handle the incoming data.

--- test/source/RS9110_UART_Test.cpp
+++ test/source/RS9110_UART_Test.cpp
@@ -53,39 +53,222 @@ void RS9110_UART_Test::CompareStream (const char *stream, unsigned int size)
 void RS9110_UART_Test::ProcessMessageTest ()
 {
     bool bRtn;
+    int respLen;
 
 
     bRtn = rs->ProcessMessage("OK\r\n", 4);
     CPPUNIT_ASSERT(bRtn == true);
     CPPUNIT_ASSERT(rs->GetResponseType() == RS9110_UART::RESP_TYPE_OK);
     CPPUNIT_ASSERT(rs->GetErrorCode() == RS9110_UART::ERROR_NONE);
-    CPPUNIT_ASSERT(rs->GetResponse(NULL) == 0);
+    rs->GetResponse(respLen);
+    CPPUNIT_ASSERT(respLen == 0);
 
     bRtn = rs->ProcessMessage("ERROR\x87\r\n", 8);
     CPPUNIT_ASSERT(bRtn == true);
     CPPUNIT_ASSERT(rs->GetResponseType() == RS9110_UART::RESP_TYPE_ERROR);
     CPPUNIT_ASSERT(rs->GetErrorCode() == RS9110_UART::ERROR_NO_EXIST_TCP_SERVER);
-    CPPUNIT_ASSERT(rs->GetResponse(NULL) == 0);
+    rs->GetResponse(respLen);
+    CPPUNIT_ASSERT(respLen == 0);
 
-    bRtn = rs->ProcessMessage("AT+RSI_READ14\x00\xC0\xA8\x01\x01\x41\x1F abcd\r\n", 27);
+    bRtn = rs->ProcessMessage("AT+RSI_READ\x0E\x00\xC0\xA8\x01\x01\x41\x1F abcd\r\n", 26);
     CPPUNIT_ASSERT(bRtn == true);
     CPPUNIT_ASSERT(rs->GetResponseType() == RS9110_UART::RESP_TYPE_READ);
-    CPPUNIT_ASSERT(rs->GetResponse(NULL) == 14);
+    rs->GetResponse(respLen);
+    CPPUNIT_ASSERT(respLen == 13);
     
-    bRtn = rs->ProcessMessage("AT+RSI_CLOSE\r\n", 11);
+    bRtn = rs->ProcessMessage("AT+RSI_CLOSE\r\n", 14);
     CPPUNIT_ASSERT(bRtn == true);
     CPPUNIT_ASSERT(rs->GetResponseType() == RS9110_UART::RESP_TYPE_CLOSE);
-    CPPUNIT_ASSERT(rs->GetResponse(NULL) == 0);
+    rs->GetResponse(respLen);
+    CPPUNIT_ASSERT(respLen == 0);
 
     bRtn = rs->ProcessMessage("SLEEP\r\n", 7);
     CPPUNIT_ASSERT(bRtn == true);
     CPPUNIT_ASSERT(rs->GetResponseType() == RS9110_UART::RESP_TYPE_SLEEP);
-    CPPUNIT_ASSERT(rs->GetResponse(NULL) == 0);
+    rs->GetResponse(respLen);
+    CPPUNIT_ASSERT(respLen == 0);
 
     bRtn = rs->ProcessMessage("UNKNOWN\r\n", 9);
     CPPUNIT_ASSERT(bRtn == false);
     CPPUNIT_ASSERT(rs->GetResponseType() == RS9110_UART::RESP_TYPE_MAX);
-    CPPUNIT_ASSERT(rs->GetResponse(NULL) == -1);
+    rs->GetResponse(respLen);
+    CPPUNIT_ASSERT(respLen == -1);
+}
+
+
+void RS9110_UART_Test::GetResponseTest ()
+{
+    bool bRtn;
+    int  respLen;
+
+
+    /* GetNumScanResults */
+    RS9110_UART::TNumScanResults *numScanResults;
+    bRtn = rs->ProcessMessage("OK\x0A\r\n", 5);
+    CPPUNIT_ASSERT(bRtn == true);
+    numScanResults = (RS9110_UART::TNumScanResults *) rs->GetResponse(respLen);
+    CPPUNIT_ASSERT(respLen == 1);
+    CPPUNIT_ASSERT(numScanResults->value == 10);
+
+    /* Scan */
+    RS9110_UART::TScan *scan;
+    bRtn = rs->ProcessMessage("OKRedpine_net1\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x14Redpine_net2\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x0E\r\n", 72);
+    CPPUNIT_ASSERT(bRtn == true);
+    scan = (RS9110_UART::TScan *) rs->GetResponse(respLen);
+    CPPUNIT_ASSERT(respLen == (sizeof(RS9110_UART::TScan) * 2));
+    CPPUNIT_ASSERT(strcmp(scan->ssid, "Redpine_net1") == 0);
+    CPPUNIT_ASSERT(scan->mode == RS9110_UART::SEC_MODE_WPA2);
+    CPPUNIT_ASSERT(scan->rssi == 0x14);
+    scan++;
+    CPPUNIT_ASSERT(strcmp(scan->ssid, "Redpine_net2") == 0);
+    CPPUNIT_ASSERT(scan->mode == RS9110_UART::SEC_MODE_WPA2);
+    CPPUNIT_ASSERT(scan->rssi == 0x0E);
+
+    /* GetMACOfAPs */
+    RS9110_UART::TBssid *bssid;
+    bRtn = rs->ProcessMessage("OKRedpine_net\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x23\xA7\x1B\x8D\x31\r\n", 42);
+    CPPUNIT_ASSERT(bRtn == true);
+    bssid = (RS9110_UART::TBssid *) rs->GetResponse(respLen);
+    CPPUNIT_ASSERT(respLen == sizeof(RS9110_UART::TBssid));
+    CPPUNIT_ASSERT(strcmp(bssid->ssid, "Redpine_net") == 0);
+    CPPUNIT_ASSERT(memcmp(bssid->bssid, "\x00\x23\xA7\x1B\x8D\x31", sizeof(bssid->bssid)) == 0);
+
+    /* GetNetworkType */
+    RS9110_UART::TNetworkType *networkType;
+    bRtn = rs->ProcessMessage("OKRedpine_net\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x01\r\n", 37);
+    CPPUNIT_ASSERT(bRtn == true);
+    networkType = (RS9110_UART::TNetworkType *) rs->GetResponse(respLen);
+    CPPUNIT_ASSERT(respLen == sizeof(RS9110_UART::TNetworkType));
+    CPPUNIT_ASSERT(strcmp(networkType->ssid, "Redpine_net") == 0);
+    CPPUNIT_ASSERT(networkType->nwType == RS9110_UART::NW_TYPE_IBSS);
+
+    /* IPConfiguration */
+    RS9110_UART::TIPConfig *ipConfig;
+    bRtn = rs->ProcessMessage("OK\x00\x23\xA7\x1B\x8D\x31\x01\x02\x03\x04\x05\x06\x07\x08\x09\x10\x11\x12\r\n", 22);
+    CPPUNIT_ASSERT(bRtn == true);
+    ipConfig = (RS9110_UART::TIPConfig *) rs->GetResponse(respLen);
+    CPPUNIT_ASSERT(respLen == sizeof(RS9110_UART::TIPConfig));
+    CPPUNIT_ASSERT(memcmp(ipConfig->mac,     "\x00\x23\xA7\x1B\x8D\x31", sizeof(ipConfig->mac))     == 0);
+    CPPUNIT_ASSERT(memcmp(ipConfig->address, "\x01\x02\x03\x04",         sizeof(ipConfig->address)) == 0);
+    CPPUNIT_ASSERT(memcmp(ipConfig->subnet,  "\x05\x06\x07\x08",         sizeof(ipConfig->subnet))  == 0);
+    CPPUNIT_ASSERT(memcmp(ipConfig->gateway, "\x09\x10\x11\x12",         sizeof(ipConfig->gateway)) == 0);
+
+    /* OpenTcpSocket and all socket openers */
+    RS9110_UART::TSocket *socket;
+    bRtn = rs->ProcessMessage("OK\x01\r\n", 5);
+    CPPUNIT_ASSERT(bRtn == true);
+    socket = (RS9110_UART::TSocket *) rs->GetResponse(respLen);
+    CPPUNIT_ASSERT(respLen == sizeof(RS9110_UART::TSocket));
+    CPPUNIT_ASSERT(socket->id == 0x01);
+
+    /* GetSocketStatus */
+    RS9110_UART::TSocketStatus *socketStatus;
+    bRtn = rs->ProcessMessage("OK\x07\xC0\xA8\x28\x0A\x41\x1F\r\n", 11);
+    CPPUNIT_ASSERT(bRtn == true);
+    socketStatus = (RS9110_UART::TSocketStatus *) rs->GetResponse(respLen);
+    CPPUNIT_ASSERT(respLen == sizeof(RS9110_UART::TSocketStatus));
+    CPPUNIT_ASSERT(socketStatus->id == 0x07);
+    CPPUNIT_ASSERT(memcmp(socketStatus->address, "\xC0\xA8\x28\x0A", sizeof(socketStatus->address)) == 0);
+    CPPUNIT_ASSERT(socketStatus->port == 0x1F41);
+
+    /* Incoming data */
+    RS9110_UART::TReadUDP readUDP;
+    bRtn = rs->ProcessMessage("AT+RSI_READ\x01\x05\x00\xC0\xA8\x01\x01\x41\x1F abcd\r\n", 27);
+    CPPUNIT_ASSERT(bRtn == true);
+    rs->Read(readUDP);
+    CPPUNIT_ASSERT(readUDP.socketId == 0x01);
+    CPPUNIT_ASSERT(readUDP.size == 0x0005);
+    CPPUNIT_ASSERT(memcmp(readUDP.address, "\xC0\xA8\x01\x01", 4) == 0);
+    CPPUNIT_ASSERT(readUDP.srcPort == 0x1F41);
+    CPPUNIT_ASSERT(memcmp(readUDP.data, " abcd", readUDP.size) == 0);
+
+    RS9110_UART::TReadTCP readTCP;
+    bRtn = rs->ProcessMessage("AT+RSI_READ\x01\x05\x00 abcd\r\n", 21);
+    CPPUNIT_ASSERT(bRtn == true);
+    rs->Read(readTCP);
+    CPPUNIT_ASSERT(readTCP.socketId == 0x01);
+    CPPUNIT_ASSERT(readTCP.size == 0x0005);
+    CPPUNIT_ASSERT(memcmp(readTCP.data, " abcd", readTCP.size) == 0);
+
+    /* GetDNS */
+    RS9110_UART::TDNSGet *dnsGet;
+    bRtn = rs->ProcessMessage("OK\x03\x0C\xA8\x0A\x0A\xC0\xA8\x0A\x14\xC0\xA8\x0A\x1E\r\n", 17);
+    CPPUNIT_ASSERT(bRtn == true);
+    dnsGet = (RS9110_UART::TDNSGet *) rs->GetResponse(respLen);
+    CPPUNIT_ASSERT(dnsGet->numIPs == 0x03);
+    CPPUNIT_ASSERT(memcmp(dnsGet->address[0], "\x0C\xA8\x0A\x0A", sizeof(dnsGet->address[0])) == 0);
+    CPPUNIT_ASSERT(memcmp(dnsGet->address[1], "\xC0\xA8\x0A\x14", sizeof(dnsGet->address[0])) == 0);
+    CPPUNIT_ASSERT(memcmp(dnsGet->address[2], "\xC0\xA8\x0A\x1E", sizeof(dnsGet->address[0])) == 0);
+
+    /* GetFirmwareVersion */
+    RS9110_UART::TFWVersion *fwVersion;
+    bRtn = rs->ProcessMessage("OK4.7.1\r\n", 9);
+    CPPUNIT_ASSERT(bRtn == true);
+    fwVersion = (RS9110_UART::TFWVersion *) rs->GetResponse(respLen);
+    CPPUNIT_ASSERT(respLen == sizeof(RS9110_UART::TFWVersion));
+    CPPUNIT_ASSERT(memcmp(fwVersion->version, "4.7.1", sizeof(fwVersion->version)) == 0);
+
+    /* GetNetworkParameters */
+    RS9110_UART::TNetworkParams *nwParams;
+    bRtn = rs->ProcessMessage("OKRedpine_net\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x01Redpine_net_password\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x06\x01\x02\x03\x04\x05\x06\x01\xC0\xA8\x32\x02\xC0\xA8\x32\x01\xFF\xFF\xFF\x00\x02\x01\x00\x1F\x41\x41\x1F\x01\x02\x03\x04\x02\x04\x1A\xF1\x41\x4C\x05\x06\x07\x08\r\n", 110);
+    CPPUNIT_ASSERT(bRtn == true);
+    nwParams = (RS9110_UART::TNetworkParams *) rs->GetResponse(respLen);
+    CPPUNIT_ASSERT(respLen == 106);
+    CPPUNIT_ASSERT(strcmp(nwParams->ssid, "Redpine_net") == 0);
+    CPPUNIT_ASSERT(nwParams->secMode == RS9110_UART::SEC_MODE_WPA);
+    CPPUNIT_ASSERT(strcmp(nwParams->psk, "Redpine_net_password") == 0);
+    CPPUNIT_ASSERT(nwParams->channel == RS9110_UART::CHANNEL_24_6);
+    CPPUNIT_ASSERT(memcmp(nwParams->mac, "\x01\x02\x03\x04\x05\x06", sizeof(nwParams->mac)) == 0);
+    CPPUNIT_ASSERT(nwParams->dhcpMode == RS9110_UART::DHCP_DHCP);
+    CPPUNIT_ASSERT(memcmp(nwParams->address, "\xC0\xA8\x32\x02", sizeof(nwParams->address)) == 0);
+    CPPUNIT_ASSERT(memcmp(nwParams->subnet,  "\xC0\xA8\x32\x01", sizeof(nwParams->subnet))  == 0);
+    CPPUNIT_ASSERT(memcmp(nwParams->gateway, "\xFF\xFF\xFF\x00", sizeof(nwParams->gateway)) == 0);
+    CPPUNIT_ASSERT(nwParams->numOpenSockets == 0x02);
+    CPPUNIT_ASSERT(nwParams->socketDetails[0].id == 0x01);
+    CPPUNIT_ASSERT(nwParams->socketDetails[0].type == RS9110_UART::SOCKET_TCP);
+    CPPUNIT_ASSERT(nwParams->socketDetails[0].srcPort == 0x411F);
+    CPPUNIT_ASSERT(nwParams->socketDetails[0].dstPort == 0x1F41);
+    CPPUNIT_ASSERT(memcmp(nwParams->socketDetails[0].dstAddress, "\x01\x02\x03\x04", sizeof(nwParams->socketDetails[0].dstAddress)) == 0);
+    CPPUNIT_ASSERT(nwParams->socketDetails[1].id == 0x02);
+    CPPUNIT_ASSERT(nwParams->socketDetails[1].type == RS9110_UART::SOCKET_LUDP);
+    CPPUNIT_ASSERT(nwParams->socketDetails[1].srcPort == 0xF11A);
+    CPPUNIT_ASSERT(nwParams->socketDetails[1].dstPort == 0x4C41);
+    CPPUNIT_ASSERT(memcmp(nwParams->socketDetails[1].dstAddress, "\x05\x06\x07\x08", sizeof(nwParams->socketDetails[1].dstAddress)) == 0);
+
+    /* GetMACAddress */
+    RS9110_UART::TMACAddress *mac;
+    bRtn = rs->ProcessMessage("OK\x00\x23\xA7\x1B\x8D\x31\r\n", 10);
+    CPPUNIT_ASSERT(bRtn == true);
+    mac = (RS9110_UART::TMACAddress *) rs->GetResponse(respLen);
+    CPPUNIT_ASSERT(respLen == sizeof(RS9110_UART::TMACAddress));
+    CPPUNIT_ASSERT(memcmp(mac->mac, "\x00\x23\xA7\x1B\x8D\x31", sizeof(mac->mac)) == 0);
+
+    /* GetRSSI */
+    RS9110_UART::TRSSI *rssi;
+    bRtn = rs->ProcessMessage("OK\x14\r\n", 5);
+    CPPUNIT_ASSERT(bRtn == true);
+    rssi = (RS9110_UART::TRSSI *) rs->GetResponse(respLen);
+    CPPUNIT_ASSERT(respLen == sizeof(RS9110_UART::TRSSI));
+    CPPUNIT_ASSERT(rssi->value == 0x14);
+
+    /* GetConfiguration */
+    RS9110_UART::TStoredConfig *config;
+    bRtn = rs->ProcessMessage("OK\x00\x01\x06\x00\x02\x04\x02 Redpine_net_password\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00 Redpine_net\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x01\x01\x02\x03\x04\x05\x06\x07\x08\x09\x10\x11\x12\xAA\xBB\xCC\xDD\r\n", 127);
+    CPPUNIT_ASSERT(bRtn == true);
+    config = (RS9110_UART::TStoredConfig *) rs->GetResponse(respLen);
+    CPPUNIT_ASSERT(config->isValid == 0x0100);
+    CPPUNIT_ASSERT(config->channel == RS9110_UART::CHANNEL_24_6);
+    CPPUNIT_ASSERT(config->nwType == RS9110_UART::NW_TYPE_RSP_INFRA);
+    CPPUNIT_ASSERT(config->secMode == RS9110_UART::SEC_MODE_RSP_WPA2);
+    CPPUNIT_ASSERT(config->dataRate == RS9110_UART::TX_RATE_11);
+    CPPUNIT_ASSERT(config->powerLevel == RS9110_UART::TX_POWER_HIGH);
+    CPPUNIT_ASSERT(strcmp(config->psk, " Redpine_net_password") == 0);
+    CPPUNIT_ASSERT(strcmp(config->ssid, " Redpine_net") == 0);
+    CPPUNIT_ASSERT(config->dhcp == RS9110_UART::DHCP_DHCP);
+    CPPUNIT_ASSERT(memcmp(config->address, "\x01\x02\x03\x04", sizeof(config->address)) == 0);
+    CPPUNIT_ASSERT(memcmp(config->subnet,  "\x05\x06\x07\x08", sizeof(config->subnet)) == 0);
+    CPPUNIT_ASSERT(memcmp(config->gateway, "\x09\x10\x11\x12", sizeof(config->gateway)) == 0);
+    CPPUNIT_ASSERT(memcmp(config->featureSelect, "\xAA\xBB\xCC\xDD", sizeof(config->featureSelect)) == 0);
 }
 
 
@@ -180,7 +363,7 @@ void RS9110_UART_Test::ScanTest ()
     CPPUNIT_ASSERT(bRtn == true);
     CPPUNIT_ASSERT(rs->GetLastCommand() == RS9110_UART::CMD_SCAN);
     CPPUNIT_ASSERT(rs->GetResponseType() == RS9110_UART::RESP_TYPE_MAX);
-    CompareStream("AT+RSI_SCAN=7\r\n");
+    CompareStream("AT+RSI_SCAN=6\r\n");
 
     bRtn = rs->Scan(12, "Redpine_net");
     CPPUNIT_ASSERT(bRtn == true);
